06c9e70b781d8b977b5d5c9fb58814cc1a3b2d5d
gcrown95 <75559011+gcrown95@users.noreply.github.com>
2023-09-30 02:25:22 -0700

    Pedro's changes

--- Bucles.py
+++ Bucles.py
@@ -4,3 +4,6 @@ animales=["aranas", "perros","gatos", "saltamontes", "osos","leones", "tigres"]
 # First foor loop
 for element in animales:
     print(element)
+    
+# Corrections on list proper
+print([x.title() for x in animales])
