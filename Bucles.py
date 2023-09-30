50bfa78ef34934e60b9ad775d0fe3d626ce0de66
gcrown95 <75559011+gcrown95@users.noreply.github.com>
2023-09-30 01:57:39 -0700

    Changes by Lucio

--- Bucles.py
+++ Bucles.py
@@ -1,6 +1,9 @@
 # List of elements to iter
 animales=["aranas", "perros","gatos", "saltamontes", "osos","leones", "tigres"]
-
+color = ["naranja", "rojo", "verde", "azul","negro"]
 # First foor loop
 for element in animales:
     print(element)
+    
+for unit in color:
+    print(unit)
